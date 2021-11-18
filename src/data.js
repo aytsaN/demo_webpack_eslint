bbbb208e53c1f7026971c7e4fef1c0e76d2f0dfe
aytsaN <aytsan.shamal@gmail.com>
2021-11-19 02:39:23 +0300

    fix: fix data.json  according prettier

--- src/data.js
+++ src/data.js
@@ -1,15 +1,14 @@
 // import json from './assets/json/data';
 
 const body = document.body;
-const body2 = document.body;
-
+// const body2 = document.body;
 
 async function getData() {
-  const response = await fetch('./assets/json/data.json')
+  const response = await fetch('./assets/json/data.json');
   let json = await response.json();
-  console.log('commits', json)
+  console.log('commits', json);
 
-  json.forEach(element => {
+  json.forEach((element) => {
     const myImage = new Image(100, 200);
     const myImage2 = new Image(100, 200);
     myImage.src = `./assets/image-data/full/${element.imageNum}full.jpg`;
